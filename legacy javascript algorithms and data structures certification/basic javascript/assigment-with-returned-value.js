351afc9355f71dbd64ec27a022820a3210eb8407
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-15 14:50:16 +0700

    Update assigment-with-returned-value.js

--- legacy javascript algorithms and data structures certification/basic javascript/assigment-with-returned-value.js
+++ legacy javascript algorithms and data structures certification/basic javascript/assigment-with-returned-value.js
@@ -1,3 +1,14 @@
+/*
+Everything to the right of the equal sign is resolved before the value is assigned. 
+This means we can take the return value of a function and assign it to a variable.
+Assume we have defined a function sum which adds two numbers together.
+
+ourSum = sum(5, 12);
+
+Calling the sum function with the arguments of 5 and 12 produces a return value of 17.
+This return value is assigned to the ourSum variable.
+*/
+
 let processed = 0;
 
 function processArg(num) {
