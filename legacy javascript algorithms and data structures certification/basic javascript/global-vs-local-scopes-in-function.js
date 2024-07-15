bd1216f1c9ad9368e4d6413cd1f52ec8112f327e
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-15 14:55:43 +0700

    Update global-vs-local-scopes-in-function.js

--- legacy javascript algorithms and data structures certification/basic javascript/global-vs-local-scopes-in-function.js
+++ legacy javascript algorithms and data structures certification/basic javascript/global-vs-local-scopes-in-function.js
@@ -1,10 +1,13 @@
-// Setup
+/*
+Global vs. Local Scope in Functions
+It is possible to have both local and global variables with the same name. 
+When you do this, the local variable takes precedence over the global variable.
+*/
+
 const outerWear = "T-Shirt";
 
 function myOutfit() {
-  // Only change code below this line
   const outerWear = "sweater";
-  // Only change code above this line
   return outerWear;
 }
 
