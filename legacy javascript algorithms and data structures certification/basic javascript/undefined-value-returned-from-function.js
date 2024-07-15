7bbd03b8e2c5c4a94f33cc0eae540874f9120eb8
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-15 14:57:33 +0700

    Update undefined-value-returned-from-function.js

--- legacy javascript algorithms and data structures certification/basic javascript/undefined-value-returned-from-function.js
+++ legacy javascript algorithms and data structures certification/basic javascript/undefined-value-returned-from-function.js
@@ -1,3 +1,7 @@
+/*
+A function can include the return statement but it does not have to.
+In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
+*/
 let sum = 0;
 
 function addThree() {
