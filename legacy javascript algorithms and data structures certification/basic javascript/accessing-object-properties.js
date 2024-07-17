3ccc140a6508fcc65d203ae55abba3c9050835a8
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-17 09:00:45 +0700

    Create accessing-object-properties.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/accessing-object-properties.js
@@ -0,0 +1,16 @@
+/*
+Accessing Object Properties with Dot Notation
+
+There are two ways to access the properties of an object: dot notation (.) and bracket notation ([]), similar to an array.
+
+Dot notation is what you use when you know the name of the property you're trying to access ahead of time.
+*/
+
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats"
+};
+
+const hatValue = testObj.hat;      
+const shirtValue = testObj.shirt;    
