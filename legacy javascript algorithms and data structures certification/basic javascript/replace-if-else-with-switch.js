ce29dd2e4ee3da5bb716a97ee48e8d4ac8bc58a1
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-17 07:15:02 +0700

    Create replace-if-else-with-switch.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/replace-if-else-with-switch.js
@@ -0,0 +1,44 @@
+/*
+Replacing If Else Chains with Switch
+
+If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. 
+The following:
+*/
+
+function chainToSwitch(val) {
+  let answer = "";
+
+  /*
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }*/
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+
+console.log(chainToSwitch(7));
