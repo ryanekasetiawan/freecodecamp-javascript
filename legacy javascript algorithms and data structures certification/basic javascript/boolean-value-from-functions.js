f05eb4f49eac160161dee6e98b5ef69d3cd012a5
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-17 07:21:40 +0700

    Create boolean-value-from-functions.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/boolean-value-from-functions.js
@@ -0,0 +1,11 @@
+/*
+Returning Boolean Values from Functions
+
+You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+*/
+
+function isLess(a, b) {
+  return a < b;
+}
+
+console.log(isLess(10, 15));
