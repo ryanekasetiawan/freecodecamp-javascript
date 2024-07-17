002b3572ce8ee656c85e2f601dc767110c798039
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-17 08:56:54 +0700

    Create objects.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/objects.js
@@ -0,0 +1,16 @@
+/*
+Build JavaScript Objects
+
+You may have heard the term object before.
+
+Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+*/
+  
+const myCat = {
+  name: "Joni",
+  legs: 4,
+  tails: 1,
+  friends: ["Black", "Jack"]
+};
