d35aa268ec99af80e9dd227c8991f7c2d9bc194b
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-19 17:17:27 +0700

    Create iterate-with-while-loops.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/iterate-with-while-loops.js
@@ -0,0 +1,14 @@
+/*
+Iterate with JavaScript While Loops
+You can run the same code multiple times by using a loop.
+
+The first type of loop we will learn is called a while loop because it runs while a specified condition is true and stops once that condition is no longer true.
+*/
+
+const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+  console.log(myArray.push(i));
+  i--;
+}
