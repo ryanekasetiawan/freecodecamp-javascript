4ce46b253a3a415133b42927b081d64878d6f2db
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-19 08:41:27 +0700

    Create access-nested-objects.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/access-nested-objects.js
@@ -0,0 +1,19 @@
+/*
+Accessing Nested Objects
+
+The sub-properties of objects can be accessed by chaining together the dot or bracket notation.
+*/
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
