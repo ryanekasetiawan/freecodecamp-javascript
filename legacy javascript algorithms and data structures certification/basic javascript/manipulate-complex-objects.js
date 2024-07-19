04f9c759e99cfc3972cf36c01db0b215e2985269
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-19 08:21:14 +0700

    Create manipulate-complex-objects.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/manipulate-complex-objects.js
@@ -0,0 +1,32 @@
+/*
+Manipulating Complex Objects
+
+Sometimes you may want to store data in a flexible Data Structure.
+A JavaScript object is one way to handle flexible data.
+They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
+*/
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Wahyu",
+    "title": "Drummer",
+    "release_year": 2000,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
