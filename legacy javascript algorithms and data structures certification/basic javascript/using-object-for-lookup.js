1e31511e85f0d20fa46668dc3105e41a76b5a660
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-19 07:53:58 +0700

    Create using-object-for-lookup.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/using-object-for-lookup.js
@@ -0,0 +1,40 @@
+/*
+
+*/
+
+function phoneticLookup(val) {
+  let result = "";
+
+  /*switch(val) {
+    case "alpha":
+      result = "Adams";
+      break;
+    case "bravo":
+      result = "Boston";
+      break;
+    case "charlie":
+      result = "Chicago";
+      break;
+    case "delta":
+      result = "Denver";
+      break;
+    case "echo":
+      result = "Easy";
+      break;
+    case "foxtrot":
+      result = "Frank";
+  }*/
+  
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+
+  result = lookup[val];
+  return result;
+}
+console.log(phoneticLookup("charlie"));
