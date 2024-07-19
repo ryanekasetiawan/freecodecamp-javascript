8b7341e4ef05aec61c623c9ee2e2f57defccf7fc
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-19 07:08:20 +0700

    Create add-new-properties-to-object.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/add-new-properties-to-object.js
@@ -0,0 +1,15 @@
+/*
+Add New Properties to a JavaScript Object
+
+You can add new properties to existing JavaScript objects the same way you would modify them.
+*/
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+console.log(myDog);
