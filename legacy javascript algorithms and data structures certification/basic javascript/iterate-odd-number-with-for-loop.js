34b59397deb0e6cfb724f7ee8a44412dc93eb3bc
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-22 10:39:57 +0700

    Create iterate-odd-number-with-for-loop.js

---
+++ legacy javascript algorithms and data structures certification/basic javascript/iterate-odd-number-with-for-loop.js
@@ -0,0 +1,12 @@
+/*
+Iterate Odd Numbers With a For Loop
+
+For loops don't have to iterate one at a time.
+By changing our final-expression, we can count by even numbers.
+*/
+
+const myArray = [];
+
+for (let i = 1; i < 10; i+=2) {
+  myArray.push(i);
+}
