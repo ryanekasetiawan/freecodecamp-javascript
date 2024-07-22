8c865a73e879e82bf2b8c9be1dab14e3d0b15f92
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-23 06:58:15 +0700

    Create use-destructuring-assignment-to-assign-variables-from-objects.js

---
+++ legacy javascript algorithms and data structures certification/ES6/use-destructuring-assignment-to-assign-variables-from-objects.js
@@ -0,0 +1,26 @@
+/*
+Use Destructuring Assignment to Assign Variables from Objects
+
+Destructuring allows you to assign a new variable name when extracting values. 
+You can do this by putting the new name after a colon when assigning the value.
+
+Using the same object from the last example:
+
+const user = { name: 'John Doe', age: 34 };
+
+Here's how you can give new variable names in the assignment:
+
+const { name: userName, age: userAge } = user;
+
+You may read it as "get the value of user.name and assign it to a new variable named userName" and so on.
+The value of userName would be the string John Doe, and the value of userAge would be the number 34.
+*/
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+  
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES; 
