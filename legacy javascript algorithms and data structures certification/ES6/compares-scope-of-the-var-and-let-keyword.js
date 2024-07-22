00aed2acc68b92b25f54be8199ae0e4cd553abaf
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-07-22 18:37:54 +0700

    Create compares-scope-of-the-var-and-let-keyword.js

---
+++ legacy javascript algorithms and data structures certification/ES6/compares-scope-of-the-var-and-let-keyword.js
@@ -0,0 +1,21 @@
+/*
+Compare Scopes of the var and let Keywords
+
+If you are unfamiliar with let, check out this challenge about the difference between let and var.
+
+When you declare a variable with the var keyword, it is declared globally, or locally if declared inside a function.
+
+The let keyword behaves similarly, but with some extra features. When you declare a variable with the let keyword inside a block, statement, or expression, its scope is limited to that block, statement, or expression.
+*/
+
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+console.log(checkScope());
